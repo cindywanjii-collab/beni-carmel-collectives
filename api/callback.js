be2c711363bd777b9bd03ce705020848e059478d
cindywanjii-collab <cindy.wanjii@mpesafoundationacademy.ac.ke>
2026-08-20 14:31:49 +0300

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,30 +1,34 @@
-module.exports = async function handler(
-    req,
-    res
-) {
+module.exports = async function handler(req, res) {
+    try {
+        console.log("M-PESA CALLBACK:");
 
-    console.log(
-        "M-PESA CALLBACK:"
-    );
+        console.log(
+            JSON.stringify(req.body, null, 2)
+        );
 
-    console.log(
-        JSON.stringify(
-            req.body,
-            null,
-            2
-        )
-    );
+        // Only accept POST requests
+        if (req.method !== "POST") {
+            return res.status(405).json({
+                ResultCode: 1,
+                ResultDesc: "Method Not Allowed"
+            });
+        }
 
-
-    return res
-        .status(200)
-        .json({
-
+        // M-Pesa expects a successful callback response
+        return res.status(200).json({
             ResultCode: 0,
-
-            ResultDesc:
-                "Accepted"
-
+            ResultDesc: "Accepted"
         });
 
+    } catch (error) {
+        console.error(
+            "M-PESA CALLBACK ERROR:",
+            error
+        );
+
+        return res.status(500).json({
+            ResultCode: 1,
+            ResultDesc: "Callback processing failed"
+        });
+    }
 };
